cad5c80288fa75bde705f450dc83a907b5309427
MDRepos <dawod.1989@yahoo.com>
2021-06-13 20:43:47 +0200

    backgroung image fixed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+          <h1>gooo</h1>
+
     </div>
-    <router-view/>
   </div>
 </template>
 
@@ -17,19 +16,17 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #03D930;
+  margin: 0;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
+
+body{
+  background-image: url('./assets/background.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
