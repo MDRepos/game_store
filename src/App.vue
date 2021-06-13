7711684e2c06188c8eb330778e1d98a1b5f14ac9
MDRepos <dawod.1989@yahoo.com>
2021-06-13 18:56:15 +0200

    font, background image added

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,11 @@
 </template>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Orbitron&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family:'Orbitron', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
