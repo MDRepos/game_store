2c58e9a745b3bc24c9557ed7db2a6f6a2af5f014
MDRepos <dawod.1989@yahoo.com>
2021-06-29 12:34:40 +0200

    sprint added

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div id="app">
     <div>
-          <h1>gooo</h1>
-
+      <home></home>
     </div>
   </div>
 </template>
 
-<style>
+<script>
+import Home from "./views/Home.vue";
+export default {
+  Components: { Home },
+  data() {
+    return {
+      cart: [],
+    };
+  },
+};
+</script>
 
-@import url('https://fonts.googleapis.com/css2?family=Orbitron&display=swap');
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Orbitron&display=swap");
 
 #app {
-  font-family:'Orbitron', Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Orbitron", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #03D930;
+  color: #03d930;
   margin: 0;
   padding: 0;
 }
 
-
-body{
-  background-image: url('./assets/background.jpg');
+body {
+  background-image: url("./assets/background.jpg");
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-size: cover;
 }
-
 </style>
